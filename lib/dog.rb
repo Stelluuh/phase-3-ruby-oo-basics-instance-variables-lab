99e5c71c56176163ed67177d8a837fd0d8fe38ff
stella <sftamesis13@gmail.com>
2022-12-06 13:46:38 -0800

    commit tree f3da5e948cb9d4881cdc7a9e5d50c831b2a634aa

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    #Sets dogs name (SETTING)
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    #Get the dogs name/reports the name (GETTING)
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
